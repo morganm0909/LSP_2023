26df06631db302b3377cdc96a39cbb8362db23e6
morganm0909 <104795413+morganm0909@users.noreply.github.com>
2023-01-24 20:48:03 -0500

    test commit

--- lsp-spr2023/src/org/howard/edu/hw1/HelloWorld.java
+++ lsp-spr2023/src/org/howard/edu/hw1/HelloWorld.java
@@ -1,10 +1,14 @@
 package org.howard.edu.hw1;
-
+/**
+ * 
+ * @author morganmartin
+ *
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello world");
 	}
 
 }
